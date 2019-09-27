10717d68a1e924c98f899b8efb719b7ef7a4fca3
zach harwood <31451701+zah8756@users.noreply.github.com>
2019-09-27 01:12:04 -0400

    wowe got rid of comments

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,4 +1,4 @@
-const fs = require('fs');  // pull in the file system module
+const fs = require('fs');  
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
--- src/server.js
+++ src/server.js
@@ -1,61 +1,43 @@
-const http = require('http'); // pull in the http server module
-const url = require('url'); // pull in the url module
-// pull in the query string module
+const http = require('http');
+const url = require('url'); 
+
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
-// pull in our json response handler file
+
 const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// const urlStruct = {
-//   '/': htmlHandler.getIndex,
-//   '/getUsers': jsonHandler.getUsers,
-//   '/notReal': jsonHandler.notReal,
-//   notFound: jsonHandler.notFound,
-// };
-
-// handle POST requests
 const handlePost = (request, response, parsedUrl) => {
-  // if post is to /addUser (our only POST url)
+
   if (parsedUrl.pathname === '/addUser') {
     const res = response;
 
-    // uploads come in as a byte stream that we need
-    // to reassemble once it's all arrived
+
     const body = [];
 
-    // if the upload stream errors out, just throw a
-    // a bad request and send it back
+
     request.on('error', (err) => {
       console.dir(err);
       res.statusCode = 400;
       res.end();
     });
 
-    // on 'data' is for each byte of data that comes in
-    // from the upload. We will add it to our byte array.
     request.on('data', (chunk) => {
       body.push(chunk);
     });
 
-    // on end of upload stream.
     request.on('end', () => {
-      // combine our byte array (using Buffer.concat)
-      // and convert it to a string value (in this instance)
+
       const bodyString = Buffer.concat(body).toString();
-      // since we are getting x-www-form-urlencoded data
-      // the format will be the same as querystrings
-      // Parse the string into an object by field name
+
       const bodyParams = query.parse(bodyString);
 
-      // pass to our addUser function
       jsonHandler.addUser(request, res, bodyParams);
     });
   }
 };
 
-// handle GET requests
 const handleGet = (request, response, parsedUrl) => {
   if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
@@ -79,12 +61,9 @@ const handleHead = (request, response, parsedUrl) => {
 };
 
 const onRequest = (request, response) => {
-  // parse url into individual parts
-  // returns an object of url parts by name
+
   const parsedUrl = url.parse(request.url);
 
-  // check if method was POST, otherwise assume GET
-  // for the sake of this example
   if (request.method === 'POST') {
     handlePost(request, response, parsedUrl);
   } else if (request.method === 'GET') {
